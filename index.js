3016a6942ee61ed7bd59122d1aa824c5e0641bdc
student <kmac.kimaru93@gmail.com>
2024-03-20 06:44:19 +0300

    adding arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+//function expression called divide
+const divide = function (a, b) {
+  return a / b;
+};
+divide(2000 / 100);
+
+//arrow function square x by itself
+const square = (x) => x * x;
+square(2);
+
+//arrow function add two numbers
+const add = (a, b) => {
+  return a + b;
+};
+add(3, 4);
